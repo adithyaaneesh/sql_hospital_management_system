cf4c1a2092b1a0c462b5c3f0b5c4f9e4d9756a54
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-11-18 12:55:00 +0530

    Order By

---
+++ order_by.sql
@@ -0,0 +1,5 @@
+select * from patient order by first_name asc;
+select * from doctor order by specialization asc;
+select * from room order by price_per_day desc;
+select * from appointment order by appointment_date asc;
+select * from patient order by age asc;
