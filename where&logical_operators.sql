152969ffb8a72d8164e4ea0a24bc1f8e0521f943
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-11-18 12:37:20 +0530

    WHERE & Logical Operators

---
+++ where&logical_operators.sql
@@ -0,0 +1,5 @@
+select * from patient where gender = 'Male';
+select * from patient where age > 25 and age <50;
+select * from doctor where specialization like '%Cardio%';
+select * from appointment where status = 'scheduled';
+select * from patient where city != 'chennai';
