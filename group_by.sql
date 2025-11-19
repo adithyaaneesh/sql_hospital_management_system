f45b8c4308497968f8f94e07f22e3edaf0a70965
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-11-19 10:10:49 +0530

    Group By

---
+++ group_by.sql
@@ -0,0 +1,6 @@
+select specialization, count(*) from doctor group by specialization;
+select specialization, count(*) from doctor group by specialization;
+select city, count(*) as total_patients from patient group by city;
+select status, count(*) as status_count from appointment group by status;
+select room_type, count(*) as total_available_rooms from room group by room_type;
+select specialization, count(*) as total_doctors from doctor group by specialization;
